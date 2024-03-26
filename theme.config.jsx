43393d6f333494b06ad4f47ad4cd30ae417887d0
João Vitor Chaves Silva <jvchavessilva@gmail.com>
2024-03-25 21:14:36 -0300

    Initial Commit

---
+++ theme.config.jsx
@@ -0,0 +1,31 @@
+export default {
+      logo: <span>Implementing The War Of Art - made with Lumentis</span>,
+      editLink: {
+        component: null,
+      },
+      project: {
+        link: "https://github.com/hrishioa/lumentis",
+      },
+      feedback: {
+        content: null,
+      },
+      footer: {
+        text: (
+          <>
+            Made with 🫶 by&nbsp;
+            <a href="https://twitter.com/hrishioa" target="_blank">
+              Hrishi - say hi!
+            </a>
+          </>
+        ),
+      },
+      head: (
+        <>
+          <meta property="og:title" content="Implementing The War Of Art" />
+          <meta property="og:description" content="PrimarySource appears to be the full text of a book titled The War of Art by Steven Pressfield. The book discusses the creative process and overcoming inner obstacles to achieve artistic goals. It seems to be written as a self-help style guide for aspiring artists, writers, entrepreneurs and anyone pursuing creative endeavors." />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/HebeHH/lumentis/choose-favicon/assets/default-favicon.png" />
+        </>
+      ),
+    };
+    
